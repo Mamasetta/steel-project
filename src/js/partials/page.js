6eeb3848fbd1bbcfb3d945f8583a62ec40afecad
Varvara Bazhukova <mamasetta@gmail.com>
2017-03-02 18:47:03 +0500

    skroll

--- src/js/partials/page.js
+++ src/js/partials/page.js
@@ -55,6 +55,129 @@ $(document).ready(function(){
       });
   });
 
-  $("#services-scroll").scrollTo(document.getElementById('services'), 800);
+  $("#header-services-link").on("click", function() {
 
+      $("html, body").animate({scrollTop: $('#services').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-benefits-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#benefits').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-econom-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#econom').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-works-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#works').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-costs-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#costs').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-stages-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#stages').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-feedback-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#feedback').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-repair-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#repair').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#header-contacts-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#contacts').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-services-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#services').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-benefits-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#benefits').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-econom-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#econom').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-works-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#works').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-costs-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#costs').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-stages-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#stages').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-feedback-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#feedback').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-repair-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#repair').offset().top}, 800);
+      return false;
+
+  })
+
+  $("#footer-contacts-link").on("click", function() {
+
+      $("html, body").animate({scrollTop: $('#contacts').offset().top}, 800);
+      return false;
+
+  })
 });
